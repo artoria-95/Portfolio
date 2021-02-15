02bb649bd6b33300caf69a72d468865832792918
artoria-95 <64420864+artoria-95@users.noreply.github.com>
2021-02-15 18:21:14 -0300

    projects section started

---
+++ portfolio/src/Components/Projects/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './styles.css';
+
+export default function Projects(){
+
+  return(
+    
+    <div className="project-container">
+      <div className="project">
+        <h3>E-Commerce</h3>
+        <p>Description</p>
+        <button>View Project</button>
+        <button>View Repository</button>
+      </div>
+      <div className="project">
+        <h3>HENRY App</h3>
+        <p>Description</p>
+        <button>View Project</button>
+        <button>View Repository</button>
+      </div>
+    </div>
+    
+  )
+}
