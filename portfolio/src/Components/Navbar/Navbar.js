6b71d6d12219832df8b2704df4cb9673b233eb69
artoria-95 <64420864+artoria-95@users.noreply.github.com>
2021-02-15 18:05:52 -0300

    scroll resolved

--- portfolio/src/Components/Navbar/Navbar.js
+++ portfolio/src/Components/Navbar/Navbar.js
@@ -22,6 +22,14 @@ export default function Navbar() {
             <FaIcons.FaLinkedin className="in" />
           </a>
         </div>
+        {/* <div>
+          <ul className="naveg">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div> */}
         <div className="stuff">
           <MDIcons.MdLanguage className="lang" />
           <FaIcons.FaLowVision className="dalt" />
