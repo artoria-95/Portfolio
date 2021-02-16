8aa151c93e0e075a7d5f8f4f14c2f6727d1640c0
artoria-95 <64420864+artoria-95@users.noreply.github.com>
2021-02-16 11:54:30 -0300

    switch branch

--- portfolio/src/Components/Navbar/Navbar.js
+++ portfolio/src/Components/Navbar/Navbar.js
@@ -7,34 +7,26 @@ import * as MDIcons from "react-icons/md";
 import "./navbar.css";
 
 export default function Navbar() {
+
+  
   return (
-    <>
+    <div>
       <div className="navbar">
         <div className="media">
           <a href="https://github.com/artoria-95" target="__blank">
             <FaIcons.FaGithubSquare className="gh" />
           </a>
 
-          <a
-            href="https://www.linkedin.com/in/ana-paula-barua-188013131/"
-            target="__blank"
-          >
+          <a href="https://www.linkedin.com/in/ana-paula-barua-188013131/"
+            target="__blank">
             <FaIcons.FaLinkedin className="in" />
           </a>
         </div>
-        {/* <div>
-          <ul className="naveg">
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#projects">Projects</a></li>
-            <li><a href="#contact">Contact</a></li>
-          </ul>
-        </div> */}
         <div className="stuff">
           <MDIcons.MdLanguage className="lang" />
           <FaIcons.FaLowVision className="dalt" />
         </div>
-      </div>
-    </>
+    </div>
+    </div>
   );
 }
