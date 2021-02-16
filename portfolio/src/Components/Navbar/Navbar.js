91f3e7e79605a447ff305b27eb10d493a68461dd
artoria-95 <64420864+artoria-95@users.noreply.github.com>
2021-02-16 15:39:15 -0300

    dropups settled

--- portfolio/src/Components/Navbar/Navbar.js
+++ portfolio/src/Components/Navbar/Navbar.js
@@ -5,31 +5,48 @@ import * as CgIcons from "react-icons/cg";
 import * as FaIcons from "react-icons/fa";
 import * as MDIcons from "react-icons/md";
 import * as IoIcons from "react-icons/io";
-import {useTranslation} from 'react-i18next';
+import { useTranslation } from "react-i18next";
 
 import "./navbar.css";
 
 export default function Navbar() {
-
   return (
-    
-      <div className="navbar">
-        <div className="media">
-          <a href="https://github.com/artoria-95" target="__blank">
-            <FaIcons.FaGithubSquare className="gh" />
-          </a>
+    <div className="navbar">
+      <div className="media">
+        <a href="https://github.com/artoria-95" target="__blank">
+          <FaIcons.FaGithubSquare className="gh" />
+        </a>
 
-          <a href="https://www.linkedin.com/in/ana-paula-barua-188013131/"
-            target="__blank">
-            <FaIcons.FaLinkedin className="in" />
-          </a>
-        </div>
-        <div className="stuff">
-          <MDIcons.MdLanguage className="lang" />
-          <FaIcons.FaLowVision className="dalt" />
-          <a href="#home"><IoIcons.IoIosArrowDropupCircle className="top" /></a>
-        </div>
+        <a
+          href="https://www.linkedin.com/in/ana-paula-barua-188013131/"
+          target="__blank"
+        >
+          <FaIcons.FaLinkedin className="in" />
+        </a>
       </div>
-    
+      <div className="stuff">
+        <div className="dropup">
+          <div className="dropbtn">
+            <MDIcons.MdLanguage className="lang" />
+          </div>
+          <div className="dropup-content">
+            <a href="#">English</a>
+            <a href="#">Spanish</a>
+          </div>
+        </div>
+<div className="dropup">
+          <div className="dropbtn">
+        <FaIcons.FaLowVision className="dalt" />
+          </div>
+          <div className="dropup-content">
+            <a href="#">Colorblind mode</a>
+            <a href="#">Regular theme</a>
+          </div>
+        </div>
+        <a href="#home">
+          <IoIcons.IoIosArrowDropupCircle className="top" />
+        </a>
+      </div>
+    </div>
   );
 }
