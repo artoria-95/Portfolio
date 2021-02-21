84f5306c6649fe14a396f79c34ba837c08283a30
artoria-95 <64420864+artoria-95@users.noreply.github.com>
2021-02-20 22:21:49 -0300

    about

--- portfolio/src/Components/About/index.js
+++ portfolio/src/Components/About/index.js
@@ -1,25 +1,32 @@
 import React from "react";
 import "./styles.css";
-import {useTranslation} from 'react-i18next';
-
+import { useTranslation } from "react-i18next";
+import * as FaIcons from "react-icons/fa";
+import * as SiIcons from "react-icons/si";
 export default function About() {
   const [t, i18n] = useTranslation("global");
 
   return (
     <div className="div1">
       <div className="div2">
-      <p>
-      {t("about.text1")}
-      <br/>
-      {t("about.text2")}
-      </p>
-      <a href="#projects"><button className="abtn">{t("about.boton")}</button></a>
-      </div>
-      <div className="dev">
-        <img
-          src="https://media-exp1.licdn.com/dms/image/C4E03AQHPxkhz29TPeg/profile-displayphoto-shrink_200_200/0/1611190654795?e=1618444800&v=beta&t=sFR3PAmdpPe-lHsKgsu1rQSlKGRFD4gX40g0CxV-smE"
-          alt="ana"
-        />
+        <p>
+          {t("about.text1")}
+          <br />
+          {t("about.text2")}
+          <br />
+          <div className="ic">
+            <i><FaIcons.FaHtml5 /></i>
+            <i><FaIcons.FaCss3Alt /></i>
+            <i><FaIcons.FaNodeJs /></i>
+            <i><FaIcons.FaReact /></i>
+            <i><SiIcons.SiRedux /></i>
+            <i><SiIcons.SiPostgresql /></i>
+            <i><SiIcons.SiMongodb /></i>
+          </div>
+        </p>
+        <a href="#projects">
+          <button className="abtn">{t("about.boton")}</button>
+        </a>
       </div>
     </div>
   );
--- portfolio/src/Components/Home/index.js
+++ portfolio/src/Components/Home/index.js
@@ -1,22 +1,35 @@
 import React from "react";
 import {useTranslation} from 'react-i18next';
+import {useSpring, animated} from 'react-spring';
 import "./styles.css";
 
 export default function Home() {
   
+  const props = useSpring({opacity: 1, from: {opacity: 0}});
 const [t, i18n] = useTranslation("global");
 
   return (
     <div className="div1">
       
-      <div className="div2">
+      <animated.div className="div2" style={props}>
         <h1>{t("home.title")}</h1>
         <p>
           {t("home.intro")}
         </p>
+        
+      </animated.div>
+      <animated.div className="dev" style={props}>
+        
+        <img
+          src="https://media-exp1.licdn.com/dms/image/C4E03AQHPxkhz29TPeg/profile-displayphoto-shrink_200_200/0/1611190654795?e=1618444800&v=beta&t=sFR3PAmdpPe-lHsKgsu1rQSlKGRFD4gX40g0CxV-smE"
+          className="ana"
+        />
+        <div class="glow-wrap">
+    <i class="glow"></i>
+  </div>
         <a href="#about"><button className="hbtn">{t("home.boton")}</button></a>
         
-      </div>
+      </animated.div>
     </div>
   );
 }
